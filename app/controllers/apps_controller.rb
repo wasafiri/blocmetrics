6c56191f2b6029fd743e6d4938a2beae22c48a6e
David Bahar <wasafiri@users.noreply.github.com>
2014-07-15 00:17:38 -0400

    Correct formatting in apps_controller

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -10,49 +10,49 @@ class AppsController < ApplicationController
 	end
 
 	def show
-		@app = App.find(params[:id])
-		authorize @app
+	  @app = App.find(params[:id])
+	  authorize @app
 	end
 
 	def create
-		@app = App.new(app_params)
-    authorize @app
-		@app.user = current_user
-    	if @app.save
-			redirect_to app_path(@app), notice: "App was added successfully." and return
-		else
-			flash[:error] = "Error adding app. Please try again."
-			render :new and return
-		end
+	  @app = App.new(app_params)
+          authorize @app
+	  @app.user = current_user
+    	    if @app.save
+	      redirect_to app_path(@app), notice: "App was added successfully." and return
+            else
+	      flash[:error] = "Error adding app. Please try again."
+	      render :new and return
+	  end
 	end
 
 	def edit
-		@app = App.find(params[:id])
-    authorize @app
-		# edit action is responsible for rendering the view
-		# update action is responsible for interacting with the model
+	  @app = App.find(params[:id])
+          authorize @app
+	  # edit action is responsible for rendering the view
+	  # update action is responsible for interacting with the model
 	end
 
 	def update
-		@app = App.find(params[:id])
-    authorize @app
-		if @app.update_attributes(app_params)
-			redirect_to @app and return
-		else
-			flash[:error] = "Error updating app. Please try again."
-			render :edit and return
-		end
+	  @app = App.find(params[:id])
+          authorize @app
+            if @app.update_attributes(app_params)
+	      redirect_to @app and return
+	    else
+	      flash[:error] = "Error updating app. Please try again."
+	      render :edit and return
+	    end
 	end
 
 	def destroy
-		@app = App.find(params[:id])
-    authorize @app
-		if @app.destroy
-			flash[:notice] = "The app #{@app.name} was successfully deleted."
-			redirect_to apps_path and return
-		else
-			flash[:error] = "There was an error deleting the app."
-		end
+	  @app = App.find(params[:id])
+          authorize @app
+	    if @app.destroy
+	      flash[:notice] = "The app #{@app.name} was successfully deleted."
+	      redirect_to apps_path and return
+	    else
+	      flash[:error] = "There was an error deleting the app."
+	    end
 	end
 
 	private
