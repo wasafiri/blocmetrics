ae1ccf6f9f4909c2801e4db8d4f94eb9ec011810
David Bahar <wasafiri@users.noreply.github.com>
2014-07-16 12:43:21 -0400

    Correct indentation in apps_controller.rb

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,7 +1,7 @@
 class AppsController < ApplicationController
 
 	def index
-		@apps = App.all
+	  @apps = App.all
 	end
 
 	def new
@@ -16,50 +16,50 @@ class AppsController < ApplicationController
 
 	def create
 	  @app = App.new(app_params)
-    authorize @app
+          authorize @app
 	  @app.user = current_user
-    if @app.save
-      redirect_to app_path(@app), notice: "App was added successfully." and return
-    else
-      flash[:error] = "Error adding app. Please try again."
-      render :new and return
+          if @app.save
+            redirect_to app_path(@app), notice: "App was added successfully." and return
+          else
+            flash[:error] = "Error adding app. Please try again."
+            render :new and return
 	  end #wyas elooking at something else
 	end
 
 	def edit
 	  @app = App.find(params[:id])
-    authorize @app
+          authorize @app
 	  # edit action is responsible for rendering the view
 	  # update action is responsible for interacting with the model
 	end
 
 	def update
 	  @app = App.find(params[:id])
-    authorize @app
-    if @app.update_attributes(app_params)
-      redirect_to @app and return
-    else
-      flash[:error] = "Error updating app. Please try again."
+          authorize @app
+          if @app.update_attributes(app_params)
+            redirect_to @app and return
+          else
+            flash[:error] = "Error updating app. Please try again."
 	    render :edit and return
 	  end
 	end
 
 	def destroy
 	  @app = App.find(params[:id])
-    authorize @app
+          authorize @app
 	  if @app.destroy
-      flash[:notice] = "The app #{@app.name} was successfully deleted."
-      redirect_to apps_path and return
-    else
-      flash[:error] = "There was an error deleting the app."
-    end
-	end
+            flash[:notice] = "The app #{@app.name} was successfully deleted."
+            redirect_to apps_path and return
+          else
+            flash[:error] = "There was an error deleting the app."
+          end
+  	end
 
 	private
 
 	def app_params
-		params.require(:app).permit(:name, :desc, :user_id)
-		# using strong_parameters here
-		# more: http://blog.sensible.io/2013/08/17/strong-parameters-by-example.html
+	  params.require(:app).permit(:name, :desc, :user_id)
+	  # using strong_parameters here
+	  # more: http://blog.sensible.io/2013/08/17/strong-parameters-by-example.html
 	end
 end
